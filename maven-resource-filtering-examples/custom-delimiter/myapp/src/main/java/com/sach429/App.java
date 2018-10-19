ce07248423c8d4e820a52229b0e5ef9722870569
Satrajit Acharyya <satrajit.acharya@gmail.com>
2018-10-18 21:21:12 -0400

    Custom filter example

---
+++ maven-resource-filtering-examples/custom-delimiter/myapp/src/main/java/com/sach429/App.java
@@ -0,0 +1,13 @@
+package com.sach429;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
